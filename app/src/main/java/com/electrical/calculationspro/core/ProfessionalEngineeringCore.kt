a0a0af5a521eb6d736ae8190f1192e0855c920d2
elghaty <elghaty@gmail.com>
2026-09-15 14:27:19 +0300

    Refactor ProfessionalEngineeringCore with new calculators

--- app/src/main/java/com/electrical/calculationspro/core/ProfessionalEngineeringCore.kt
+++ app/src/main/java/com/electrical/calculationspro/core/ProfessionalEngineeringCore.kt
@@ -1,38 +1,81 @@
 package com.electrical.calculationspro.core
 
-import com.electrical.calculationspro.core.calculators.DiversityCalculator
-import com.electrical.calculationspro.core.calculators.PanelCalculator
-import com.electrical.calculationspro.core.calculators.ShortCircuitCalculator
-import com.electrical.calculationspro.core.calculators.SldGenerator
-import com.electrical.calculationspro.core.calculators.SldShortCircuitCalculator
+import com.electrical.calculationspro.core.calculation.BreakerCalculator
+import com.electrical.calculationspro.core.calculation.CableCalculator
+import com.electrical.calculationspro.core.calculation.DesignSummaryCalculator
+import com.electrical.calculationspro.core.calculation.ElectricalNetworkCalculator
+import com.electrical.calculationspro.core.calculation.GeneratorCalculator
+import com.electrical.calculationspro.core.calculation.LoadCalculator
+import com.electrical.calculationspro.core.calculation.LoadScheduleCalculator
+import com.electrical.calculationspro.core.calculation.MotorCalculator
+import com.electrical.calculationspro.core.calculation.PumpCalculator
+import com.electrical.calculationspro.core.calculation.ShortCircuitCalculator
+import com.electrical.calculationspro.core.calculation.TransformerCalculator
+import com.electrical.calculationspro.core.calculation.VoltageDropCalculator
+import com.electrical.calculationspro.core.calculation.PowerCalculator
 
 /**
- * PROFESSIONAL ENGINEERING CORE
+ * SINGLE ENGINEERING FACADE.
  *
- * Single logical engineering calculation core.
+ * All engineering calculations must enter through this object.
  *
- * UI
- *   ↓
- * ProfessionalEngineeringFacade
- *   ↓
- * ProfessionalEngineeringCore
- *   ↓
- * Engineering Calculators
+ * UI and ViewModels must not contain engineering formulas.
  */
-class ProfessionalEngineeringCore {
+class ProfessionalEngineeringCore private constructor() {
 
-    val diversity =
-        DiversityCalculator()
+    val power =
+        PowerCalculator()
+
+    val loads =
+        LoadCalculator()
+
+    val loadSchedule =
+        LoadScheduleCalculator(loads)
+
+    val voltageDrop =
+        VoltageDropCalculator()
 
     val shortCircuit =
         ShortCircuitCalculator()
 
-    val panel =
-        PanelCalculator()
+    val breakers =
+        BreakerCalculator()
 
-    val sld =
-        SldGenerator()
+    val cables =
+        CableCalculator(
+            power = power,
+            voltageDrop = voltageDrop,
+            shortCircuit = shortCircuit,
+            breaker = breakers
+        )
 
-    val sldShortCircuit =
-        SldShortCircuitCalculator()
+    val transformers =
+        TransformerCalculator()
+
+    val generators =
+        GeneratorCalculator()
+
+    val motors =
+        MotorCalculator()
+
+    val pumps =
+        PumpCalculator()
+
+    val designSummary =
+        DesignSummaryCalculator(
+            schedule = loadSchedule
+        )
+
+    val network =
+        ElectricalNetworkCalculator(
+            summary = designSummary
+        )
+
+    companion object {
+
+        val instance:
+            ProfessionalEngineeringCore by lazy {
+                ProfessionalEngineeringCore()
+            }
+    }
 }
