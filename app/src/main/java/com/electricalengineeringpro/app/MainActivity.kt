7ff41cc28490d0337fea6622747696650148dbbf
elghaty <elghaty@gmail.com>
2026-09-15 11:24:31 +0300

    Add MainActivity with basic Compose setup

---
+++ app/src/main/java/com/electricalengineeringpro/app/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.electricalengineeringpro.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.electricalengineeringpro.app.ui.AppRoot
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface {
+                    AppRoot()
+                }
+            }
+        }
+    }
+}
