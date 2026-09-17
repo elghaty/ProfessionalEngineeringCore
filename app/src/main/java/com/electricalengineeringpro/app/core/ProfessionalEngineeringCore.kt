536ae6e33595fe4fc5e4d7a70c77a4a8c410fe87
elghaty <elghaty@gmail.com>
2026-09-17 12:32:57 +0300

    Update ProfessionalEngineeringCore.kt

--- app/src/main/java/com/electricalengineeringpro/app/core/ProfessionalEngineeringCore.kt
+++ app/src/main/java/com/electricalengineeringpro/app/core/ProfessionalEngineeringCore.kt
@@ -3,9 +3,9 @@ package com.electricalengineeringpro.app.core
 import com.electricalengineeringpro.app.core.calculation.BreakerCalculator
 import com.electricalengineeringpro.app.core.calculation.BreakerSelectionCalculator
 import com.electricalengineeringpro.app.core.calculation.CableCalculator
+import com.electricalengineeringpro.app.core.calculation.CompleteDesignCalculator
 import com.electricalengineeringpro.app.core.calculation.DesignSummaryCalculator
 import com.electricalengineeringpro.app.core.calculation.ElectricalNetworkCalculator
-import com.electricalengineeringpro.app.core.calculation.EngineeringDesignService
 import com.electricalengineeringpro.app.core.calculation.GeneratorCalculator
 import com.electricalengineeringpro.app.core.calculation.LoadCalculator
 import com.electricalengineeringpro.app.core.calculation.LoadScheduleCalculator
@@ -18,46 +18,69 @@ import com.electricalengineeringpro.app.core.calculation.ShortCircuitCalculator
 import com.electricalengineeringpro.app.core.calculation.TransformerCalculator
 import com.electricalengineeringpro.app.core.calculation.TransformerSizingCalculator
 import com.electricalengineeringpro.app.core.calculation.VoltageDropCalculator
+import com.electricalengineeringpro.app.core.sld.SldGenerator
 
 class ProfessionalEngineeringCore private constructor() {
 
-    val power = PowerCalculator()
+    val power =
+        PowerCalculator()
 
-    val loads = LoadCalculator()
+    val loads =
+        LoadCalculator()
 
-    val designSummary = DesignSummaryCalculator()
+    val designSummary =
+        DesignSummaryCalculator()
 
-    val loadSchedule = LoadScheduleCalculator(loads)
+    val loadSchedule =
+        LoadScheduleCalculator(loads)
 
-    val cable = CableCalculator()
+    val cable =
+        CableCalculator()
 
-    val breaker = BreakerCalculator()
+    val breaker =
+        BreakerCalculator()
 
-    val breakerSelection = BreakerSelectionCalculator()
+    val breakerSelection =
+        BreakerSelectionCalculator()
 
-    val voltageDrop = VoltageDropCalculator()
+    val voltageDrop =
+        VoltageDropCalculator()
 
-    val shortCircuit = ShortCircuitCalculator()
+    val shortCircuit =
+        ShortCircuitCalculator()
 
-    val transformer = TransformerCalculator()
+    val transformer =
+        TransformerCalculator()
 
-    val transformerSizing = TransformerSizingCalculator()
+    val transformerSizing =
+        TransformerSizingCalculator()
 
-    val generators = GeneratorCalculator()
+    val generators =
+        GeneratorCalculator()
 
-    val motors = MotorCalculator()
+    val motors =
+        MotorCalculator()
 
-    val pumps = PumpCalculator()
+    val pumps =
+        PumpCalculator()
 
-    val protection = ProtectionCalculator()
+    val protection =
+        ProtectionCalculator()
 
-    val mdb = MdbCalculator()
+    val mdb =
+        MdbCalculator()
 
-    val network = ElectricalNetworkCalculator(loads)
+    val network =
+        ElectricalNetworkCalculator(loads)
 
-    val completeDesign = EngineeringDesignService(this)
+    val completeDesign =
+        CompleteDesignCalculator(this)
+
+    val sld =
+        SldGenerator()
 
     companion object {
+
         val instance: ProfessionalEngineeringCore by lazy {
             ProfessionalEngineeringCore()
         }
