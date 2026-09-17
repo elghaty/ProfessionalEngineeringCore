39f1415a06f498348d633388f8a44e6620715ed6
elghaty <elghaty@gmail.com>
2026-09-17 11:07:36 +0300

    Delete app/src/main/java/com/electricalengineeringpro/app/ui directory

--- app/src/main/java/com/electricalengineeringpro/app/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.electricalengineeringpro.app.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val EngineeringBlue = Color(0xFF123B63)
-val EngineeringBlueDark = Color(0xFF08243D)
-val EngineeringSurface = Color(0xFFF5F7FA)
-val EngineeringAccent = Color(0xFF1976A8)
-val EngineeringText = Color(0xFF17212B)
