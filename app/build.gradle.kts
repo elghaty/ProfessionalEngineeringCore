a2c95ff8e5afac9a16220785f63db374a96edbd9
elghaty <elghaty@gmail.com>
2026-09-19 11:55:31 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,9 @@ android {
 
     defaultConfig {
         applicationId = "com.electricalengineeringpro.app.core"
-
         minSdk = 24
-
         targetSdk = 35
-
         versionCode = 1
-
         versionName = "1.0"
     }
 
@@ -33,6 +29,10 @@ android {
         buildConfig = true
     }
 
+    signingConfigs {
+        getByName("debug")
+    }
+
     buildTypes {
         getByName("debug") {
             isMinifyEnabled = false
@@ -40,15 +40,14 @@ android {
 
         getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-
     implementation("androidx.appcompat:appcompat:1.7.0")
-
     implementation("com.google.android.material:material:1.12.0")
 
     testImplementation("junit:junit:4.13.2")
