dc2aff25c2b8a863caaf15e96e6d2cab8f6279ff
elghaty <elghaty@gmail.com>
2026-09-19 09:53:43 +0300

    Refactor dependencies in build.gradle.kts
    Removed redundant dependency declarations and consolidated them.

--- ElectricalCalculationsPro/app/build.gradle.kts
+++ ElectricalCalculationsPro/app/build.gradle.kts
@@ -11,14 +11,11 @@ kotlin {
 
 android {
     namespace = "com.electrical.calculationspro"
-
     compileSdk = 35
 
     defaultConfig {
         applicationId = "com.electrical.calculationspro"
-
         minSdk = 24
-
         targetSdk = 35
 
         versionCode =
@@ -63,48 +60,18 @@ android {
 }
 
 dependencies {
-
     implementation(
         "com.github.elghaty:ProfessionalEngineeringCore:1.0.0"
     )
 
-    implementation(
-        "androidx.core:core-ktx:1.15.0"
-    )
-
-    implementation(
-        "androidx.activity:activity-compose:1.10.0"
-    )
-
-    implementation(
-        "androidx.compose.ui:ui:1.7.6"
-    )
-
-    implementation(
-        "androidx.compose.material3:material3:1.3.1"
-    )
-
-    implementation(
-        "androidx.compose.ui:ui-tooling-preview:1.7.6"
-    )
-
-    debugImplementation(
-        "androidx.compose.ui:ui-tooling:1.7.6"
-    )
-
-    implementation(
-        "androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7"
-    )
-
-    implementation(
-        "androidx.room:room-runtime:2.6.1"
-    )
-
-    implementation(
-        "androidx.room:room-ktx:2.6.1"
-    )
-
-    ksp(
-        "androidx.room:room-compiler:2.6.1"
-    )
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 }
