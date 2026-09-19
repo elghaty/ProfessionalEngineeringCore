067fa7466d5dfa2d13883e2d086b1f59b404ef4b
elghaty <elghaty@gmail.com>
2026-09-19 09:50:20 +0300

    Add build configuration for ProfessionalEngineeringCore

---
+++ ProfessionalEngineeringCore/app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+group = "com.github.elghaty"
+version = "1.0.0"
+
+android {
+    namespace = "com.electricalengineeringpro.app.core"
+
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        buildConfig = false
+    }
+
+    publishing {
+        singleVariant("release")
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.elghaty"
+                artifactId = "ProfessionalEngineeringCore"
+                version = "1.0.0"
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    testImplementation("junit:junit:4.13.2")
+}
